36127d667688a814f4118efab50309cda89ef818
Timur Turatbekov <timatur242005@gmail.com>
2024-04-13 08:25:59 +0600

    Added health component

---
+++ scripts/units/health_component.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var start_health = 100
+@onready var health = start_health
+
+func heal(amount: int) -> void:
+  health += amount
+  if health > start_health:
+    health = start_health
+
+func deal_damage(damage: int) -> void:
+  health -= damage
+  if health <= 0:
+    death()
+
+func death() -> void:
+  queue_free()
